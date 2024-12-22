c9754322f25f98e2a355f14ca418d03cbce25ca6
smrutiranjan1132001 <smruti1132001@gmail.com>
2024-12-22 20:43:37 +0530

    updated-appname and logo

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ dotenv.config();
 
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 5000
 
 app.use(express.json()); 
 app.use(cookieParser());
